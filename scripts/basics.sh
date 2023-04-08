474f6909f97c71e2fcbaeed47d8962488f5cd4b3
jgfranco17 <chino.franco@gmail.com>
2023-04-08 14:29:30 +0900

    Added`build-essential` to setup

--- scripts/basics.sh
+++ scripts/basics.sh
@@ -2,15 +2,12 @@
 gsettings set org.gnome.shell.extensions.dash-to-dock show-apps-at-top true
 sudo apt update && sudo apt upgrade
 sudo apt install -y curl
+sudo apt install build-essential
 
 # Configure dev tools
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt install -y nodejs
 sudo apt install git
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
-test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-test -r ~/.bash_profile && echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bash_profile
-echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.profile
+curl -fsSL -o install.sh https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
 brew install tree
 brew install pyenv
