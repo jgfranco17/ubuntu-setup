dba20235765303b02f5f1cfa4fde49992c705e3b
jgfranco17 <chino.franco@gmail.com>
2023-04-10 16:15:51 +0900

    Added Hashicorp, implemented confirmation echoes

--- scripts/apps.sh
+++ scripts/apps.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+echo "Preparing to install dev apps..."
 sudo apt update && sudo apt upgrade
 
 # GNOME Tweaks
 sudo add-apt-repository universe
 sudo apt install gnome-tweaks
+echo "Set up GNOME Tweaks"
 
 # VS Code
 sudo apt-get install wget gpg
@@ -14,10 +16,24 @@ rm -f packages.microsoft.gpg
 sudo apt install apt-transport-https
 sudo apt update
 sudo apt install code
+echo "Installed VS Code"
 
 # Jetbrains Toolbox
 sudo apt-get install -y libfuse2
 curl -fsSL https://raw.githubusercontent.com/nagygergo/jetbrains-toolbox-install/master/jetbrains-toolbox.sh | bash
+echo "Installed Jetbrains Toolbox"
 
 # Vtop GUI
 sudo npm install -g vtop
+echo "Installed vtop"
+
+# Hashicorp suite
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update
+sudo apt-get install nomad
+sudo apt-get install consul
+sudo apt-get install vault
+sudo apt-get install packer
+sudo apt-get install terraform
+echo "Installed Hashicorp suite"
