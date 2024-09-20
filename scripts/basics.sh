bf018ad30b4fca74de341e8d9851d81c4cdbb908
Joaquin Franco <chino.franco@gmail.com>
2024-09-20 20:59:14 +0900

    feat: add other configs and installs

--- scripts/basics.sh
+++ scripts/basics.sh
@@ -5,9 +5,20 @@ sudo apt install -y curl
 sudo apt install build-essential
 
 # Configure dev tools
-curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 sudo apt install -y nodejs
 sudo apt install git
+sudo apt-get update
+sudo apt-get install terminator
 curl -fsSL -o install.sh https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
-brew install tree
-brew install pyenv
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Browsers
+GOOGLE_CHROME_DEB="google-chrome-stable_current_amd64.deb"
+wget "https://dl.google.com/linux/direct/${GOOGLE_CHROME_DEB}"
+sudo dpkg -i "${GOOGLE_CHROME_DEB}"
+BRAVE_RELEASE_URL="https://brave-browser-apt-release.s3.brave.com"
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg ${BRAVE_RELEASE_URL}/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] ${BRAVE_RELEASE_URL}/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install brave-browser
--- scripts/shell.sh
+++ scripts/shell.sh
@@ -6,3 +6,11 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 # Set up plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+# Copy the configs to where they should be
+cp ../configs/.devops.zsh-theme $HOME/.oh-my-zsh/themes/devops.zsh-theme
+
+# Vim configuration
+mkdir -p ~/.vim ~/.vim/autoload ~/.vim/backup ~/.vim/colors ~/.vim/plugged
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
