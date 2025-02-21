47de0c125407dc25c2febda8ad9753f362c1d542
Chino Franco <chino.franco@gmail.com>
2025-02-21 23:08:25 +0900

    feat: install Go

---
+++ .github/pull_request_template.md
@@ -0,0 +1,14 @@
+<!-- markdownlint-disable-file MD041 -->
+
+# PULL REQUEST
+
+## Description
+
+<!-- High level description or summary of the PR -->
+
+### Changes
+
+<!-- List of changes -->
+
+- _Type your changes here_
+- _Type your changes here_
---
+++ scripts/languages.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -eux
+
+GO_VERSION_TO_INSTALL="1.23.2"
+GO_BIN_ARCHIVE="go${GO_VERSION_TO_INSTALL}.linux-amd64.tar.gz"
+
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go${GO_VERSION_TO_INSTALL}.linux-amd64.tar.gz"
+
+# Add Go bin to PATH if not already there
+GO_BIN_PATH="/usr/local/go/bin"
+if echo $PATH | grep -q "$GO_BIN_PATH"; then
+    echo "Go bin already in ${PATH}"
+else
+    export PATH="$PATH:/usr/local/go/bin";
+fi
+
+# Verify installation
+go version
