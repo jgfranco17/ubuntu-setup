bb31206317f8aa1081e59c0436c2999d12141dc9
Joaquin Franco <chino.franco@gmail.com>
2026-01-31 10:32:27 +0900

    fix: update tooling

--- scripts/basics.sh
+++ scripts/basics.sh
@@ -9,17 +9,15 @@ sudo apt install build-essential
 
 # Configure dev tools
 curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
-sudo apt install -y nodejs
-sudo apt install git
-sudo apt-get update
-sudo apt-get install terminator
+sudo apt install -y nodejs git alacritty
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
 
 # Browsers
 GOOGLE_CHROME_DEB="google-chrome-stable_current_amd64.deb"
+BRAVE_RELEASE_URL="https://brave-browser-apt-release.s3.brave.com"
 wget "https://dl.google.com/linux/direct/${GOOGLE_CHROME_DEB}"
 sudo dpkg -i "${GOOGLE_CHROME_DEB}"
-BRAVE_RELEASE_URL="https://brave-browser-apt-release.s3.brave.com"
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg ${BRAVE_RELEASE_URL}/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] ${BRAVE_RELEASE_URL}/ stable main"| sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
